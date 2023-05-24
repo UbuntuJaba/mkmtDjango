caa3e03cff968401489a8f8bae122f45c1a3c090
UbuntuJaba <133713849+UbuntuJaba@users.noreply.github.com>
2023-05-24 19:28:22 +0300

    Add files via upload

--- mkmtmedia/mkmt/models.py
+++ mkmtmedia/mkmt/models.py
@@ -5,14 +5,14 @@ class studyingPrograms(models.Model):
     title = models.CharField(max_length = 255, verbose_name='Программа обучения')
     formOfEdu = models.TextField(blank = True, verbose_name='Форма обучения')
     budgetPlaces = models.IntegerField(blank = True, verbose_name='Бюджетные места')
-    greenWave = models.IntegerField(blank = True, verbose_name='Зеленая волна')
+    greenWave = models.IntegerField(blank = True, null = True, verbose_name='Зеленая волна')
     passingScore = models.IntegerField(blank = True, verbose_name='Проходной балл')
     profile = models.TextField(blank = True, verbose_name='Профиль обучения')
     ege = models.TextField(blank = True, verbose_name='ЕГЭ')
     spo = models.TextField(blank = True, verbose_name='СПО')
-    priceFT = models.TextField(blank = True, verbose_name='Стоимость очного обучения')
-    pricePT = models.TextField(blank = True, verbose_name='Стоимость очно-заочного обучения')
-    priceDL = models.TextField(blank = True, verbose_name='Стоимость заочного обучения')
+    priceFT = models.TextField(blank = True, null = True, verbose_name='Стоимость очного обучения')
+    pricePT = models.TextField(blank = True, null = True, verbose_name='Стоимость очно-заочного обучения')
+    priceDL = models.TextField(blank = True, null = True, verbose_name='Стоимость заочного обучения')
 
     class Meta:
         verbose_name = 'Бакалавриат'
--- mkmtmedia/mkmt/views.py
+++ mkmtmedia/mkmt/views.py
@@ -10,7 +10,18 @@ def faculty(request):
     return render(request, 'mkmt/faculty.html')
 
 def education(request):
-    return render(request, 'mkmt/education.html')
+    undergraduate = studyingPrograms.objects.all()
+    magistr090402 = magistracy.objects.filter(title = '09.04.02 Информационные системы и технологии')
+    magistr420405 = magistracy.objects.filter(title = '42.04.05 Медиакоммуникации')
+    magistr420401 = magistracy.objects.filter(title = '42.04.01 Реклама и связи с общественностью')
+    magistr510403 = magistracy.objects.filter(title = '51.04.03 Социально-культурная деятельность')
+    magistr530402 = magistracy.objects.filter(title = '53.04.02 Вокальное искусство')
+    return render(request, 'mkmt/education.html', {'undergraduate': undergraduate, 
+                                              'magistr090402': magistr090402,
+                                              'magistr420405': magistr420405,
+                                              'magistr420401': magistr420401,
+                                              'magistr510403': magistr510403,
+                                              'magistr530402': magistr530402})
 
 
 def test(request):
