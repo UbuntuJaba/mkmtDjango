e69fab9feba909cb8f2505de6c3fa5ee017301b8
UbuntuJaba <133713849+UbuntuJaba@users.noreply.github.com>
2023-05-24 03:21:51 +0300

    Add files via upload

--- mkmtmedia/mkmt/models.py
+++ mkmtmedia/mkmt/models.py
@@ -15,9 +15,28 @@ class studyingPrograms(models.Model):
     priceDL = models.TextField(blank = True, verbose_name='Стоимость заочного обучения')
 
     class Meta:
-        verbose_name = 'Программы обучения'
-        verbose_name_plural = 'Программы обучения'
+        verbose_name = 'Бакалавриат'
+        verbose_name_plural = 'Бакалавриат'
         ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+class magistracy(models.Model):
+    title = models.CharField(max_length = 255, verbose_name='Программа обучения')
+    formOfEdu = models.TextField(blank = True, verbose_name='Форма обучения')
+    profile = models.TextField(blank = True, verbose_name='Профиль обучения')
+    priceFT = models.TextField(blank = True, null = True, verbose_name='Стоимость очного обучения')
+    pricePT = models.TextField(blank = True, null = True, verbose_name='Стоимость очно-заочного обучения')
+    priceDL = models.TextField(blank = True, null = True, verbose_name='Стоимость заочного обучения')
+    trainingPeriodFT = models.TextField(blank = True, null = True, verbose_name='Срок очного обучения')
+    trainingPeriodPT = models.TextField(blank = True, null = True, verbose_name='Срок очно-заочного обучения')
+    trainingPeriodDL = models.TextField(blank = True, null = True, verbose_name='Срок заочного обучения')
+
+    class Meta:
+        verbose_name = 'Магистратура '
+        verbose_name_plural = 'Магистратура'
+        ordering = ['id']
 
     def __str__(self):
         return self.title
