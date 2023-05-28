06dcbcf45f0f7fdc44bc18b8422130f0579c8ff7
UbuntuJaba <133713849+UbuntuJaba@users.noreply.github.com>
2023-05-28 18:31:06 +0300

    Add files via upload

--- mkmtmedia/mkmt/models.py
+++ mkmtmedia/mkmt/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class studyingPrograms(models.Model):
     title = models.CharField(max_length = 255, verbose_name='Программа обучения')
     formOfEdu = models.TextField(blank = True, verbose_name='Форма обучения')
-    budgetPlaces = models.IntegerField(blank = True, verbose_name='Бюджетные места')
+    budgetPlaces = models.TextField(blank = True, null = True, verbose_name='Бюджетные места')
     greenWave = models.IntegerField(blank = True, null = True, verbose_name='Зеленая волна')
-    passingScore = models.IntegerField(blank = True, verbose_name='Проходной балл')
+    passingScore = models.IntegerField(blank = True, null = True, verbose_name='Проходной балл')
     profile = models.TextField(blank = True, verbose_name='Профиль обучения')
     ege = models.TextField(blank = True, verbose_name='ЕГЭ')
     spo = models.TextField(blank = True, verbose_name='СПО')
@@ -25,6 +25,8 @@ class studyingPrograms(models.Model):
 class magistracy(models.Model):
     title = models.CharField(max_length = 255, verbose_name='Программа обучения')
     formOfEdu = models.TextField(blank = True, verbose_name='Форма обучения')
+    budgetPlaces = models.IntegerField(blank = True, null = True, verbose_name='Бюджетные места')
+    exam = models.TextField(blank = True, null = True, verbose_name='Вступительный экзамен')
     profile = models.TextField(blank = True, verbose_name='Профиль обучения')
     priceFT = models.TextField(blank = True, null = True, verbose_name='Стоимость очного обучения')
     pricePT = models.TextField(blank = True, null = True, verbose_name='Стоимость очно-заочного обучения')
